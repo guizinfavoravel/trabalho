aedaed8432e69dc9b9d517c7977486becdee74cc
guizinfavoravel <patolinomafioso00@gmail.com>
2025-04-07 14:24:49 -0300

    numeros

---
+++ index.js
@@ -0,0 +1,15 @@
+const Numero = (parseFloat(prompt("insira um nuemro")))
+
+if (!isNaN(Numero)) {
+
+    alert("seu numero é invalido");
+
+    if (Numero > 0) {
+        alert("seu numero é positivo")
+    }else{
+        alert("seu numero é menor que 0")
+
+    }
+
+
+}
